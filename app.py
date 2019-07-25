a5c9e1453451db9779778b23141cccea38ae7f0f
Joseph Francis <josephfrancis100@gmail.com>
2019-07-25 01:44:59 -0500

    Can enter search term and find the word in the dictionary with case insensitivity

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ import json
 
 data = json.load(open("data.json"))  # Converts to JSON to Python dictionary
 
+
+def find(word):
+    term = word.lower()
+    if term in data:
+        return data[term]
+
+    return "The given word is not in my dictionary lol"
+
+
+user_input = input("Enter the search term: ")
+print(find(user_input))
+
+
